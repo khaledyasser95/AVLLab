c5fee16e7a2b8245593632bb27e2ba551db4eb0a
amr9mohamed <amrmohamed53@gmail.com>
2017-04-29 21:28:21 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,63 +5,61 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+	// write your code here
         AvlTree avl = new AvlTree();
-        Avl_Gui x = new Avl_Gui();
         Dictionary dic = new Dictionary();
         dic.setTree(avl);
-        System.out.println("\t\t\tEnter what option you want: ");
-        System.out.print("1-Add Dictionary.txt to AVL.\n" + "2-Search queries.txt in AVL.\n" +
-                "3-Delete some words from AVL.\n" + "4-Insert 1 word in the AVL.\n" +
-                "5-Search 1 word in the AVL.\n" + "6-Delete 1 word from AVL.\n" +
-                "7-print number of words in the AVL\n" + "8-Print height of the AVL\n" + "9-GUI\n" +
-                "0-END TESTING\n");
+        System.out.println("\t\t\tEnter the option you want to do: ");
+        System.out.print("1-Add Dictionary.txt to AVL.\n"+"2-Search queries.txt in AVL.\n"+
+                         "3-Delete some words from AVL.\n"+"4-Insert 1 word in the AVL.\n"+
+                         "5-Search 1 word in the AVL.\n"+"6-Delete 1 word from AVL.\n"+
+                         "7-print number of words in the AVL\n"+"8-Print height of the AVL\n"+
+                         "9-Print the In Order Traversal\n"+"0-END TESTING\n");
         Scanner scan = new Scanner(System.in);
         int option = scan.nextInt();
         while (option != 0) {
             scan.nextLine();
             switch (option) {
-                case 1: {
+                case 1:{
                     dic.addFileToDictionary();
                     break;
                 }
-                case 2: {
+                case 2:{
                     dic.searchDictionary();
                     break;
                 }
-                case 3: {
+                case 3:{
                     dic.deleteQueries();
                     break;
                 }
-                case 4: {
+                case 4:{
                     System.out.println("Enter the word you want to insert:");
                     dic.addWord(scan.nextLine());
                     break;
                 }
-                case 5: {
+                case 5:{
                     System.out.println("Enter the word you want to search for: ");
                     dic.searchWord(scan.nextLine());
                     break;
                 }
-                case 6: {
+                case 6:{
                     System.out.println("Enter the word you want to delete: ");
                     dic.deleteWord(scan.nextLine());
                     break;
                 }
-                case 7: {
-                    System.out.println(dic.dictionarySize() + " words in the AVL");
+                case 7:{
+                    System.out.println(dic.dictionarySize()+" words in the AVL");
                     break;
                 }
-                case 8: {
+                case 8:{
                     dic.dictionaryHeight();
                     break;
                 }
-                case 9: {
-                    x.pack();
-                    x.setVisible(true);
+                case 9:{
+                    dic.inOrderTraversal(avl.getRoot());
                     break;
                 }
-                default: {
+                default:{
                     System.out.println("ENTER A VALID OPTION");
                     break;
                 }
